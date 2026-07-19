d6946734652af1c027be39a882e2fd6af0281377
shablin <maksimchick@mail.ru>
2026-07-19 16:27:22 +0300

    refactor(cmd): change start command

--- bot/handlers/start.py
+++ bot/handlers/start.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from keyboards.menu import main_menu
 
 router = Router()
 
@@ -10,4 +11,17 @@ router = Router()
 async def start(
     message: Message,
 ):
-    await message.reply("Okay. I'm working!")
+    msg = (
+        "Привет! Я смотритель Никляндии. "
+        "Пока что я умею мало, но постоянно развиваюсь.\n\n"
+        "*Подсказка:*\n"
+        "/status - узнать текущий статус сервера\n"
+        "/online - подробности об онлайн игроках\n"
+        "/go - запустить сервер"
+    )
+
+    await message.answer(
+        text=msg,
+        parse_mode="markdown",
+        reply_markup=main_menu()
+    )
